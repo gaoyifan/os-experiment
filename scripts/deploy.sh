c664c3760660422bd3e81c614c7ef21232b04bef
Yifan Gao <git@yfgao.com>
2017-05-17 21:14:35 +0800

    refact deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd book
-git init
+
 git config user.name $GIT_USER_NAME
 git config user.email $GIT_USER_EMAIL
-git add .
-git commit -m "Update Pages"
-git push -qf "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+ghp-import -n book
+git push -qf https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
